a2c3ebaef0421813085aec407105389acc7d6fb6
Prasan Hombal <phombal1@in.ibm.com>
2019-09-19 17:55:34 +0530

    my springproject

---
+++ src/main/java/com/vinu/demo/SpringBootMicroservice1Application.java
@@ -0,0 +1,14 @@
+package com.vinu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice1Application.class, args);
+		System.out.println("Welcome to SprngBoot Application");
+	}
+
+}
